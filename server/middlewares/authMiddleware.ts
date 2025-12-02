fc307e3094d51e77d56149761d8eae065d41afd5
Saurabh Yadv <ysaurav987@gmail.com>
2025-12-02 19:09:24 +0530

    added logIn, logOut, authorize middleware logic

--- server/middlewares/authMiddleware.ts
+++ server/middlewares/authMiddleware.ts
@@ -1,7 +1,42 @@
 import jwt from 'jsonwebtoken';
 import prisma from '../config/prisma';
 import type { Request, Response, NextFunction} from 'express';
+import { JWT_SECRET } from '../config/env.config';
+import { json } from 'stream/consumers';
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    
+// yet to understand how this works and where else we can use this 
+declare global {
+    namespace Express {
+        interface Request {
+            user?: any;
+        }
+    }
+}
+
+export const authorize = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let token;
+        
+        if (req.cookies && req.cookies.token) {
+            token = req.cookies.token;
+        }else if (req.headers.authorization && req.headers.authorization.startsWith("bearer")) {
+            token = req.headers.authorization.split(' ')[1]
+        }
+
+        if(!token) {
+            return res.status(401).json({message: "Unauthorised"});
+        }
+
+        const decoded = jwt.verify(token,JWT_SECRET as string) as { userId: string };
+
+        const user = await prisma.user.findUnique({ where: { id: parseInt(decoded.userId, 10) } })
+
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : "unauthorized token";
+        res.status(401).json({message: "Unauthorized", error: errorMessage})
+    }
 }
--- server/routes/authRoute.ts
+++ server/routes/authRoute.ts
@@ -1,9 +1,11 @@
 import { Router, type Request, type Response } from "express";
-import { signUp } from "../controllers/authController";
+import { signUp, logIn, logOut } from "../controllers/authController";
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', signUp); 
+authRouter.post('/log-in', logIn); 
+authRouter.post('/log-out', logOut); 
 
 export default authRouter;
 
