3a8392441483eba0e6c3da974f371aa2a6f72346
Saurabh Yadv <ysaurav987@gmail.com>
2026-01-06 16:33:22 +0530

    removed hardcoded port number

--- server/config/env.config.ts
+++ server/config/env.config.ts
@@ -3,6 +3,7 @@ import {config } from 'dotenv';
 config({path: `.env.${process.env.NODE_ENV || 'development'}.local`});
 
 export const {
+    PORT,
     NODE_ENV, GeminiApiKey,
     DATABASE_URL,
     JWT_SECRET, EXPIRES_IN,
